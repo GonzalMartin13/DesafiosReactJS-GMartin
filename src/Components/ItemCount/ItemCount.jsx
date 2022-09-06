768b8447f5d9b9597bb0baa2d537127cd97770c2
Gonzalo Martin Campo <Gonzalo.m.martin@gmail.com>
2022-09-06 01:16:16 -0300

    LimitesSolucionados

--- src/Components/ItemCount/ItemCount.jsx
+++ src/Components/ItemCount/ItemCount.jsx
@@ -4,28 +4,27 @@ const ItemCount = ({initial, cantidad}) => {
     const [inicial, setInicial] = useState(initial);
     const [stock, setStock] = useState(cantidad);
 
+    const Sumar = (valor1, valor2) => {
+        if (valor1 < cantidad){
+            setInicial(valor1 + 1);
+            setStock(valor2 -1);
+        }
+    }
+    const Restar = (valor1, valor2) => {
+        if (valor1 > 0 ) {
+            setInicial(valor1 -1);
+            setStock(valor2 +1);
+        }
+    }
+
 
     return(
-        <div className="text-center">
+        <div className="text-center ">
             <p>Suma tus unidades!</p>
             <div>
-                <button onClick={
-                    () => { 
-                        if ({inicial} !== 0 && {stock} !== 0){
-                        setInicial(inicial -1 )
-                        setStock(stock +1)
-                        }
-                    }
-                } > - </button>
+                <button onClick={() => {Restar (inicial, stock)}}> - </button>
                 <input type="text" value={inicial} className="text-center" />
-                <button onClick={
-                    () => {
-                        if ({inicial} !== 0 && {stock} !== 0){
-                            setInicial(inicial +1 )
-                            setStock(stock -1)
-                            };
-                    }
-                }> + </button>
+                <button onClick={() => {Sumar(inicial, stock)}}> + </button>
             </div>
             <p>Cantidad disponible: {stock}</p>
             <button className="mt-2">Agregar al carrito</button>
